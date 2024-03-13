48e8ec25ecbf4a8ee6bb450fc569cff2f05938f3
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-03-13 13:37:08 +0700

    Feature : Completed mini-challenge-1

---
+++ mini-challenge-1/index.js
@@ -0,0 +1,17 @@
+const aritmatika = require("./aritmatika");
+
+// Fungsi tambah
+const operasiTambah = aritmatika.tambah(5, 3);
+console.log(`Hasil Penjumlahan: ${operasiTambah}`);
+
+// Fungsi kurang
+const operasiKurang = aritmatika.kurang(10, 4);
+console.log(`Hasil Pengurangan: ${operasiKurang}`);
+
+// Fungsi kali
+const operasiKali = aritmatika.kali(6, 7);
+console.log(`Hasil Perkalian: ${operasiKali}`);
+
+// Fungsi bagi
+const operasiBagi = aritmatika.bagi(10, 2);
+console.log(`Hasil Pembagian: ${operasiBagi}`);
